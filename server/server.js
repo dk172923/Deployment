21d679e6c7ad5e613d50a1afb1d31221ba844c57
dk172923 <dhineshkumar24murugan007@gmail.com>
2024-10-02 21:34:27 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -20,7 +20,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://deployment-client.vercel.app/'
+}));
+
   
 app.use(json());
 
