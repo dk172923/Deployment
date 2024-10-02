e3ffdf9a92ea3577e76556abcef5c2f96d8fdaaf
dk172923 <dhineshkumar24murugan007@gmail.com>
2024-10-02 21:46:28 +0530

    removed cors

--- server/server.js
+++ server/server.js
@@ -20,9 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: 'https://deployment-client.vercel.app/'
-}));
+app.use(cors());
 
   
 app.use(json());
